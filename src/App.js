1d1a62e0f3af2dd6ab5f54dca4c1ba155da1be79
Michael <mikerooze12@gmail.com>
2020-02-14 20:34:34 -0800

    Add Header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
-import Header from './components/Header';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
+import { Typography } from '@material-ui/core';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Header />
-      </div>
-    );
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#000000',
+      main: '#76E5FC',
+      dark: '#65c2d4',
+      contrastText: '#274060'
+    },
+    secondary: {
+      light: '#000000',
+      main: '#5C2751',
+      dark: '#000000',
+      contrastText: '#000000'
+    }
+  },
+  typography: {
+    useNextVariants: true,
+    fontFamily: ['Spicy Rice', 'cursive']
   }
-  
+});
+
+const styles = {
+  h1: {
+    fontSize: 72,
+    fontWeight: 600,
+    margin: '2rem auto',
+    textAlign: 'center'
+  }
 }
 
-export default App;
+const App = (props) => {
+  const { classes } = props;
+  return (
+    <MuiThemeProvider theme={theme}>
+      <Typography children={'Bar Darts'} variant={'h1'} className={classes.h1}/>
+    </MuiThemeProvider>
+  );
+}
+
+export default withStyles(styles)(App);
--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const header = (props) => (
-    <h1>
-        Bar Darts
-    </h1>
-);
-
-export default header;
