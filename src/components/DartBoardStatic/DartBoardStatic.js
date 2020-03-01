b6e08b42e2f89a65b5ff073fb2fcbbd3e67a76dc
Michael <mikerooze12@gmail.com>
2020-03-01 01:39:30 -0800

    Add alert message functionality to new game button

--- src/components/DartBoardStatic/DartBoardStatic.js
+++ src/components/DartBoardStatic/DartBoardStatic.js
@@ -3,6 +3,9 @@ import DartBoardItem from './DartBoardItem/DartBoardItem';
 import { connect } from 'react-redux';
 import { resetBoard } from '../../store/actions/actions';
 import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import classes from './DartBoardStatic.module.css';
 import PlayersBox from './PlayersBox/PlayersBox';
 
@@ -17,9 +20,20 @@ const labels = [
 ];
 
 const DartBoardStatic = (props) => {
+    
     const resetBoardHandler = () => {
-        props.onResetBoard();
+        props.onResetBoard() && setOpen(false);
     }
+
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
 
     return (
         <div className="Label-style">
@@ -31,10 +45,26 @@ const DartBoardStatic = (props) => {
             />     
         ))}
         <div className={classes.newGame}>
-                <Button variant="contained" color="secondary" onClick={resetBoardHandler}>
-                    New Game
+            <Button variant="contained" color="secondary" onClick={handleClickOpen}>
+                New Game
+            </Button>
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+            >
+                <DialogTitle id="alert-dialog-title">{"Are you sure you want to Start a New Game?"}</DialogTitle>
+                
+                <DialogActions>
+                <Button onClick={handleClose} color="primary">
+                    No
                 </Button>
-            </div>
+                <Button onClick={resetBoardHandler} color="primary" autoFocus>
+                    Yes
+                </Button>
+                </DialogActions>
+            </Dialog>     
+        </div>
     </div>
     ) 
 }
